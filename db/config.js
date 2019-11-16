1fe2d6478bf93638f063b06011bec0241eb4a445
Lauren Albert <pleaseonthekeys@gmail.com>
2019-11-16 15:33:18 -0500

    changed DB name to use with docker

--- db/config.js
+++ db/config.js
@@ -1,6 +1,6 @@
 const myDatabaseConnection = require("mongoose");
 
-myDatabaseConnection.connect("mongodb://localhost:27017/SDC", {
+myDatabaseConnection.connect("mongodb://localhost:27017/SDCTest", {
   useNewUrlParser: true
 });
 
