66efb740acaf9a3c65cf25698a3a6ed76542d735
Lauren Albert <pleaseonthekeys@gmail.com>
2019-11-16 17:21:27 -0500

    changed ip address to ec2 server

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const app = express();
 const QARouter = require("./server/routes");
 
 app.use(parser.json());
-app.use("/qa", QARouter);
+app.use("http://18.224.170.190/qa", QARouter);
 
-let port = process.env.PORT || 3000;
+let port = process.env.PORT || 3030;
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
