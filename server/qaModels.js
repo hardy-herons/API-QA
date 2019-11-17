1f92d5b3d6b1660bbd9cff085ef11e59dcc38ac7
Lauren Albert <pleaseonthekeys@gmail.com>
2019-11-17 15:00:45 -0500

    reinvoked lean() with find queries

--- server/qaModels.js
+++ server/qaModels.js
@@ -4,8 +4,7 @@ const Photos = require("../db/models/photos.js");
 
 //question data
 const questionData = product_id => {
-  return Question.find({ product_id });
-  // .lean();
+  return Question.find({ product_id }).lean();
 };
 
 const qHelpfulData = question_id => {
@@ -73,8 +72,7 @@ const addQData = (product_id, req_body) => {
 
 //answer data
 const answerData = question_id => {
-  return Answers.find({ question_id });
-  // .lean();
+  return Answers.find({ question_id }).lean();
 };
 
 const aHelpfulData = answer_id => {
