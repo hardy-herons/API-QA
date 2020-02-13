474de817a74f25b45a92b5fb2bc3dfe717711ea7
Lauren Albert <pleaseonthekeys@gmail.com>
2020-02-13 17:27:24 -0500

    code clean up, deleting comments

--- read-file.js
+++ read-file.js
@@ -21,12 +21,15 @@ const giveTransformStream = (columns, transformData) =>
         skip_lines_with_error: true,
         trim: true
       },
-      (err, parse) => {
+      async (err, parse) => {
+        if (err) {
+        }
         let [parsed] = parse;
         if (row === 1) {
           row++;
           return callback();
         }
+
         const newStructure = transformData(parsed);
         this.push(newStructure);
         row++;
@@ -85,47 +88,3 @@ async function csvToNdjson({ filepath, newFilepath, columns, transformData }) {
 }
 
 module.exports = csvToNdjson;
-
-// 1. read data in chunks: readStream
-// 2. split chunks based on NLD (gives out indv rows)
-// 3. parse individual rows of data
-
-// WHY STREAMS?
-//1.instead of reading a huge (2gb) data (which is also not possible as nodejs would crash),
-//streams break that data up into many smaller parts (ex: 10,000 10mb chunks....customizable)
-//2.streams ensure that only a small size chunk is waiting in RAM before/while processing
-//the rest of the data is maintained on the disk and not taking up space in memory
-
-// 10KB
-
-// FIRSTNAME,LASTNAME,AGE
-// john,doe,23
-// tushar, s, 24
-// x, y, 12
-
-// john,doe,23
-// tushar,
-
-// ## First Chunk
-// 1 - there's  a new line char after 3
-// 2 - take that line and pass it to next writeStream ("john,doe,23")
-// 3 - store thre rest of it("tushar,")
-
-//  s, 24
-// x, y, 12
-
-// ## Second Chunk
-// 1 - there's a new line after 4
-// 2 - whatever data we have from prev chunk add that to this line and pass on to the next
-// ("tushar,"+ "s, 24") = ("tushar, s, 24")
-// 3 - store the rest of it ("")
-
-// function transformDuplexStream(rowChunk) {
-//   const parsedObject = parseCSRow(rowChunk);
-
-//   const newObj = {
-//     initialname: parsedObject.FIRNAME
-//   };
-
-//   return newObj;
-// }
